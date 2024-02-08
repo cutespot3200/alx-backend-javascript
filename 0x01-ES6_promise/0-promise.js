2e8a6a6410602ffa763dd9c2919a79eb46d855b5
cutespot3200 <id_bailey32@yahoo.com>
2024-02-08 09:54:02 +0000

    error fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
+/* eslint-disable */
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // eslint-disable-next-line no-constant-condition
-    if (true) {
-      resolve();
-    } else {
-      reject();
-    }
-  });
+  return (new Promise((resolve, reject) => {}));
 }
